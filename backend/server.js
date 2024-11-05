8dd892c5343bc4296db3737a0bfe3274e0b71697
praveen <praveenvenkatachalam2001@gmail.com>
2024-11-05 20:54:13 +0530

    Install Express.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,15 @@
-console.log("Hello World");
-console.log("Hello World");console.log("Hello World");
-console.log("Hello World");
+const express =require("express");
+const  dotenv =require ("dotenv").config();
+const app =express ();
+
+const port= process.env.PORT || 5000;
+app.get ('/api/contacts',(req,res)=>{
+     res.send("Get All Contacts");
+})
+
+app.listen(port, () =>{
+  
+     console.log('Server running on port ${port}');
+});
+
+
