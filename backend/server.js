209ea6c178116f691e51728a58d5e76fe2b7ae98
praveen <praveenvenkatachalam2001@gmail.com>
2024-11-12 21:36:36 +0530

    Some

--- backend/server.js
+++ backend/server.js
@@ -12,4 +12,20 @@ app.listen(port, () =>{
      console.log('Server running on port ${port}');
 });
 
+const express =require("express");
+const  dotenv =require ("dotenv").config();
+const app =express ();
+
+const port= process.env.PORT || 5000;
+app.get ('/api/contacts',(req,res)=>{
+     res.send("Get All Contacts");
+})
+
+app.listen(port, () =>{
+  
+     console.log('Server running on port ${port}');
+});
+
+
+
 
