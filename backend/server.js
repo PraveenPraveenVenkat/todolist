5e9149dfa56f302fb4744618ee715f368fafeb39
praveen <praveenvenkatachalam2001@gmail.com>
2024-11-26 23:04:20 +0530

    POST Request

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,24 @@
 const express =require('express');
 // const  dotenv =require ("dotenv").config();
 const app =express ();
+app.use(express.json())
 
+// Sample in-memory Storage For Todos items
+let todos = [];
 
-app.get ('/',(req,res)=>{
-     res.send("Hello World");
+//   Create a New Todo item
+ap.post('/todos',(req,res)=>{
+     const {title,description} =re.body;
+     const newTodo ={
+          id: todos.length +1,
+          title,
+          description
+     };
+     todos.push(newTodos);
+     console.log(todos);
+     re.status(201).json(newTodos);
 })
+
 // Start the Server
 const port= 3000;
 // const port =3000;
